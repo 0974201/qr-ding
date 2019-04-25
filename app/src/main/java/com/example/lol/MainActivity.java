2acf11151e676b7d32c934e76d5b82f323fc8378
toucannon <13010751@student.hhs.nl>
2019-04-25 04:22:10 +0200

    oh shit alles loopt vast send help

---
+++ app/src/main/java/com/example/lol/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //waarom heeft dit ding een spellchecker. leaf me be.
+    //todo: yeet mezelf uit een raam
+    //oh, nice ik wist niet dat android studio dat deed.
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
