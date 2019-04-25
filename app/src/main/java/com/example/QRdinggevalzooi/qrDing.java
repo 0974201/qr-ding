ef6e97473ceda8fa9dc8dbe7809fd31a55fe01f3
toucannon <13010751@student.hhs.nl>
2019-04-26 01:05:59 +0200

    hjhjjkn
    Signed-off-by: toucannon <31491170+toucannon@users.noreply.github.com>

--- app/src/main/java/com/example/QRdinggevalzooi/qrDing.java
+++ app/src/main/java/com/example/QRdinggevalzooi/qrDing.java
@@ -20,44 +20,38 @@ public class qrDing{
     //jfsdlkfjslkdjflkdfjlkdjf ik ga mezelf van het dak yeeten
     //todo: yeet mezelf van het dak
     //https://zxing.github.io/zxing/apidocs/ !!! apidocs yessssss <3
-
-    //variabelen:
-    //private int SIZE = 350;
-
+    
     public static Bitmap qrDing(String geweldigeTekst) throws WriterException {
 
         QRCodeWriter qr = new QRCodeWriter(); //de writer aanroepen om qr dingetje te maken.
 
-        //geweldigeTekst = "aaaa"; // <- meegeven wat er in een textbox wordt ingevoerd.
-        int size = 350; //hoe groot de qr code moet zijn in pixels, don't ask why i did it like this
-        int width = size;
-        int height = size;
+        int qrWH = 350; //hoe groot de qr code moet zijn in pixels
 
         HashMap<EncodeHintType, Object> encodeding = new HashMap<EncodeHintType, Object>();
         //econdjhintype in hashmap gestopt, geef object mee zodat die de andere shit pakt.
         encodeding.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.Q); //geeft error correctie mee, level q is 25% ofzo iets met correctie in qr codes moet dit nog uitzoeken op wiki ofzo
-        encodeding.put(EncodeHintType.MARGIN, 3); //maakt dat witte vlak ding om de qr code kleiner
+        encodeding.put(EncodeHintType.MARGIN, 2); //maakt dat witte vlak ding om de qr code kleiner
 
         try {
-            BitMatrix maakQRDing = qr.encode(geweldigeTekst, BarcodeFormat.QR_CODE, width, height, encodeding);
+            BitMatrix maakQRDing = qr.encode(geweldigeTekst, BarcodeFormat.QR_CODE, qrWH, qrWH, encodeding);
             //hm, oke let's see, de string geeft mee wat er encoded moet worden,
             //BarcodeFormat in welke format het moet (bijv qr, maar kan ook een barcode zijn),
             //width en height spreken voor zich
             //encodeding neemt extra shit mee zoals error correctie of wlke soort encoding (utf-8/shift-js) je wilt gebruiken
 
-            int[] zooi = new int[size * size]; //array aanmaken om de qr erin te proppen
-            for (int i = 0; i < size; i++){
-                for (int j = 0; j < size; j++){
+            int[] zooi = new int[qrWH * qrWH]; //array aanmaken om de qr erin te proppen
+            for (int i = 0; i < qrWH; i++){
+                for (int j = 0; j < qrWH; j++){
                     if (maakQRDing.get(j,i)){ //kijkt naar huidige bit en als het true is dan wordt ie zwart
-                        zooi[i * size + j] = Color.BLACK;
+                        zooi[i * qrWH + j] = Color.BLACK;
                     } else {
-                        zooi[i * size + j] = Color.WHITE;
+                        zooi[i * qrWH + j] = Color.WHITE;
                     }
                 }
             }
 
-            Bitmap yeet = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888); //qr code > bitmap zodat het gezien kan worden
-            yeet.setPixels(zooi, 0, size, 0, 0, size, size); //uh schijnbaar zorgt dit ervoor dat de qr code scherp is??
+            Bitmap yeet = Bitmap.createBitmap(qrWH, qrWH, Bitmap.Config.ARGB_8888); //qr code > bitmap zodat het gezien kan worden
+            yeet.setPixels(zooi, 0, qrWH, 0, 0, qrWH, qrWH); //uh schijnbaar zorgt dit ervoor dat de qr code scherp is??
             return yeet;
 
         } catch (WriterException ex){
