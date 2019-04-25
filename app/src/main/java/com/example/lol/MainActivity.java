064ccbc0f1b6a112c69de49522a8e5c99b19053e
toucannon <13010751@student.hhs.nl>
2019-04-25 19:51:16 +0200

    hfghf

--- app/src/main/java/com/example/lol/MainActivity.java
+++ app/src/main/java/com/example/lol/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.lol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     //waarom heeft dit ding een spellchecker. leaf me be.
     //todo: schrijf stuff idek
     //oh, nice ik wist niet dat android studio dat deed.
-    //also, waarom gebruikt het letterlijk 1GB ram. android studio why
+    //also, waarom gebruikt het letterlijk >1GB ram. android studio why
 
-    qrgen.qrDing()
+    //qrgen.qrDing()
 
     @Override
 
@@ -18,4 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //ImageView view = new ImageView();
+    //view.setImageBitmap(yeet);
 }
